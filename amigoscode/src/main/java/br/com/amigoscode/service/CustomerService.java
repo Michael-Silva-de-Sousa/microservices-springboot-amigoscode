43f222d70a4966a3729e5576461aadbea5a6e1fb
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-04 20:57:13 -0300

    V0.11 - Refatoração na injeção de dependências.

--- amigoscode/src/main/java/br/com/amigoscode/service/CustomerService.java
+++ amigoscode/src/main/java/br/com/amigoscode/service/CustomerService.java
@@ -3,10 +3,14 @@ package br.com.amigoscode.service;
 import br.com.amigoscode.persistence.Customer;
 import br.com.amigoscode.persistence.CustomerRepository;
 import br.com.amigoscode.web.CustomerRegistrationRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
--- amigoscode/src/main/java/br/com/amigoscode/web/CustomerController.java
+++ amigoscode/src/main/java/br/com/amigoscode/web/CustomerController.java
@@ -1,6 +1,7 @@
 package br.com.amigoscode.web;
 
 import br.com.amigoscode.service.CustomerService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
+@AllArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
 
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new customer registration {}", customerRegistrationRequest);
