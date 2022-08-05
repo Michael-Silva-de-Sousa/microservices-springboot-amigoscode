fd153767fdad39de58f2cb104b670b4967c332d9
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-04 21:21:06 -0300

    V0.13 - Implementado restTemplate para realizar a request Get para o microservice fraud.

--- amigoscode/src/main/java/br/com/amigoscode/service/CustomerService.java
+++ amigoscode/src/main/java/br/com/amigoscode/service/CustomerService.java
@@ -3,22 +3,41 @@ package br.com.amigoscode.service;
 import br.com.amigoscode.persistence.Customer;
 import br.com.amigoscode.persistence.CustomerRepository;
 import br.com.amigoscode.web.CustomerRegistrationRequest;
+import br.com.amigoscode.web.FraudCheckResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
+
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        ResponseEntity<FraudCheckResponse> fraudCheckResponse = restTemplate.getForEntity(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if(fraudCheckResponse.getStatusCode() == HttpStatus.OK &&
+                Objects.requireNonNull(fraudCheckResponse.getBody()).isFrauddster()){
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
