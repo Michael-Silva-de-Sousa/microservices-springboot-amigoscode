51a741dd9e24d7ea4aea40ed7ef91cbec444afdf
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-05 07:03:28 -0300

    V0.15 - Implementado POST para cadastrar o Customer.

--- fraud/src/main/java/br/com/amigoscode/web/FraudController.java
+++ fraud/src/main/java/br/com/amigoscode/web/FraudController.java
@@ -2,12 +2,14 @@ package br.com.amigoscode.web;
 
 import br.com.amigoscode.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -16,6 +18,7 @@ public class FraudController {
     public ResponseEntity<FraudCheckResponse> isFraudster(
             @PathVariable("customerId") Integer customerID) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return ResponseEntity.ok().body(new FraudCheckResponse(isFraudulentCustomer));
     }
 }
